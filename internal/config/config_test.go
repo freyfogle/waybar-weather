590a4c49db0e59516ed6c83f01421d306cbc2ff7
Winni Neessen <wn@neessen.dev>
2025-11-12 14:27:58 +0000

    Added unit tests for config initialization and validation
    - Implemented tests for `New()` to ensure defaults are properly set.
    - Added test cases for invalid environment variables handling.
    - Validated configuration fields like `forecast hours` and `units` for correctness.
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ internal/config/config_test.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"log/slog"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	const (
+		expectDefaultUnits          = "metric"
+		expectLogLevel              = slog.LevelInfo
+		expectWeatherForecastHours  = 3
+		expectIntervalWeatherUpdate = time.Minute * 15
+		expectIntervalOutput        = time.Second * 30
+	)
+	t.Run("new config with all defaults set", func(t *testing.T) {
+		conf, err := New()
+		if err != nil {
+			t.Errorf("failed to load config: %s", err)
+		}
+		if conf.Units != expectDefaultUnits {
+			t.Errorf("expected units to be: %s, got %s", expectDefaultUnits, conf.Units)
+		}
+		if conf.LogLevel != expectLogLevel {
+			t.Errorf("expected log level to be: %s, got %s", expectLogLevel, conf.LogLevel)
+		}
+		if conf.Weather.ForecastHours != expectWeatherForecastHours {
+			t.Errorf("expected weather forecast hours to be: %d, got %d", expectWeatherForecastHours,
+				conf.Weather.ForecastHours)
+		}
+		if conf.Intervals.WeatherUpdate != expectIntervalWeatherUpdate {
+			t.Errorf("expected weather update interval to be: %s, got %s", expectIntervalWeatherUpdate,
+				conf.Intervals.WeatherUpdate)
+		}
+		if conf.Intervals.Output != expectIntervalOutput {
+			t.Errorf("expected output interval to be: %s, got %s", expectIntervalOutput, conf.Intervals.Output)
+		}
+	})
+	t.Run("new config with invalid values from env", func(t *testing.T) {
+		t.Setenv("WAYBARWEATHER_LOGLEVEL", "invalid")
+		_, err := New()
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
+		}
+	})
+	t.Run("config validate forecast hours", func(t *testing.T) {
+		t.Setenv("WAYBARWEATHER_WEATHER_FORECAST_HOURS", "-1")
+		_, err := New()
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
+		}
+		t.Setenv("WAYBARWEATHER_WEATHER_FORECAST_HOURS", "25")
+		_, err = New()
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
+		}
+	})
+	t.Run("config validate units", func(t *testing.T) {
+		t.Setenv("WAYBARWEATHER_UNITS", "invalid")
+		_, err := New()
+		if err == nil {
+			t.Error("expected config to fail, but didn't")
+		}
+	})
+}
